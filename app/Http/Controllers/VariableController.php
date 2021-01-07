753f69f99e45f70e4db39745be6133266878cab0
Mba Stephane <stephane.mba@snh.cm>
2021-01-07 19:31:59 +0100

    debug changing getvariables into getVariables

--- app/Http/Controllers/VariableController.php
+++ app/Http/Controllers/VariableController.php
@@ -11,7 +11,7 @@ class VariableController extends Controller
     private $sucess_status = 200;
 
     public function getVariables(Request $request){
-        Log::info('avant avant denvoyer la reponse VariableController '. json_encode($variables));
+        Log::info('avant avant denvoyer la reponse VariableController ');
         $cle = $request->cle;
         $code = $request->code;
         $filter = [];
