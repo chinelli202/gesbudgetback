7f3ff7674fd8601ee21f727273fb39a00729371a
Mba Stephane <stephane.mba@snh.cm>
2021-01-07 14:26:09 +0100

    rolling back changes to variables controller

--- app/Http/Controllers/VariableController.php
+++ app/Http/Controllers/VariableController.php
@@ -21,8 +21,8 @@ class VariableController extends Controller
             array_push($filter, ['code', '=', $code]);
         }
         $variables = Variable::where($filter)
-            ->orderBy('code')->first();
-            //->get();
+            ->orderBy('code')
+            ->get();
 
         return response()->json(["status" => $this->sucess_status, "success" => true, "data" => $variables]);
     }
