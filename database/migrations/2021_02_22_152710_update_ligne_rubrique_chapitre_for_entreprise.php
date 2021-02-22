d021cf7121999694c7108937badf971848fae624
jp_meka <jp_meka@hotmail.com>
2021-02-22 15:36:27 +0100

    migration update elemens for entreprise

---
+++ database/migrations/2021_02_22_152710_update_ligne_rubrique_chapitre_for_entreprise.php
@@ -0,0 +1,70 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UpdateModelEntitiesForEntreprise extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return  void
+     */
+    public function up()
+    {
+        Schema::table('lignes', function (Blueprint $table) {// Add team_id column
+            $table->string('entreprise_code');
+
+            // Create foreign keys
+            $table->foreign('entreprise_code')->references('code')->on('entreprises')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+        Schema::table('rubriques', function (Blueprint $table) {// Add team_id column
+            $table->string('entreprise_code');
+
+            // Create foreign keys
+            $table->foreign('entreprise_code')->references('code')->on('entreprises')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+        Schema::table('chapitres', function (Blueprint $table) {// Add team_id column
+            $table->string('entreprise_code');
+
+            // Create foreign keys
+            $table->foreign('entreprise_code')->references('code')->on('entreprises')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+        
+        Schema::table('titres', function (Blueprint $table) {// Add team_id column
+            $table->string('entreprise_code');
+
+            // Create foreign keys
+            $table->foreign('entreprise_code')->references('code')->on('entreprises')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return  void
+     */
+    public function down()
+    {
+        Schema::table('lignes', function (Blueprint $table) {
+            $table->dropForeign(['entreprise_code']);
+        }); 
+
+        Schema::table('rubriques', function (Blueprint $table) {
+            $table->dropForeign(['entreprise_code']);
+        });
+
+        Schema::table('chapitres', function (Blueprint $table) {
+            $table->dropForeign(['entreprise_code']);
+        });
+
+        Schema::table('titres', function (Blueprint $table) {
+            $table->dropForeign(['entreprise_code']);
+        });
+    }
+}
